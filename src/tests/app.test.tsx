92a63ef7379eaf626b5ecf1574348195d580492a
Peter Sormunen <peter.sormunen@outlook.com>
2021-03-29 09:24:30 +0300

    Fix tests.

--- src/tests/app.test.tsx
+++ src/tests/app.test.tsx
@@ -1,78 +1,61 @@
-import React from "react";
 import { render, screen, fireEvent, cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import App from "../App";
 
 afterEach(() => cleanup);
 
-it("renders without crashing", () => {
-  render(<App />);
-});
+describe("todo app", () => {
+  it("renders without crashing", () => {
+    render(<App />);
+  });
 
-test("renders input for new task", () => {
-  render(<App />);
-  const inputElement = screen.getByPlaceholderText(
-    /new task/i
-  ) as HTMLInputElement;
-  expect(inputElement).toBeInTheDocument();
-});
+  it("renders input for new task", () => {
+    render(<App />);
+    const inputElement = screen.getByPlaceholderText(
+      /new task/i
+    ) as HTMLInputElement;
+    expect(inputElement).toBeInTheDocument();
+  });
 
-test("write something to input", () => {
-  render(<App />);
-  const inputElement = screen.getByPlaceholderText(
-    /new task/i
-  ) as HTMLInputElement;
-  fireEvent.change(inputElement, { target: { value: "task" } });
-  expect(inputElement.value).toBe("task");
-});
+  it("is able to handle input writing", () => {
+    render(<App />);
+    const inputElement = screen.getByPlaceholderText(
+      /new task/i
+    ) as HTMLInputElement;
+    fireEvent.change(inputElement, { target: { value: "task" } });
+    expect(inputElement.value).toBe("task");
+  });
 
-test("write something to input and submit it", () => {
-  render(<App />);
-  const inputElement = screen.getByPlaceholderText(
-    /new task/i
-  ) as HTMLInputElement;
-  expect(inputElement.value).toBe("");
-  fireEvent.change(inputElement, { target: { value: "task" } });
-  expect(inputElement.value).toBe("task");
-  fireEvent.keyDown(inputElement, { key: "Enter", code: "Enter" });
-  const todoTask = screen.getByText(/task/i);
-  expect(todoTask).toBeTruthy();
-});
+  it("is able to render written task", () => {
+    render(<App />);
+    const inputElement = screen.getByPlaceholderText(
+      /new task/i
+    ) as HTMLInputElement;
+    expect(inputElement.value).toBe("");
+    fireEvent.change(inputElement, { target: { value: "task" } });
+    expect(inputElement.value).toBe("task");
+    fireEvent.keyDown(inputElement, { key: "Enter", code: "Enter" });
+    const todoTask = screen.getByText(/task/i);
+    expect(todoTask).toBeTruthy();
+  });
 
-test("try to submit", () => {
-  render(<App />);
-  const inputElement = screen.getByPlaceholderText(
-    /new task/i
-  ) as HTMLInputElement;
-  expect(inputElement.value).toBe("");
-  fireEvent.keyDown(inputElement, { key: "Enter", code: "Enter" });
-  const errorMsg = screen.getByText(/please give a task/i);
-  expect(errorMsg).toBeTruthy();
-});
+  it("is able to warning if there is no task written in input adn we try to submit it", () => {
+    render(<App />);
+    const inputElement = screen.getByPlaceholderText(
+      /new task/i
+    ) as HTMLInputElement;
+    expect(inputElement.value).toBe("");
+    fireEvent.keyDown(inputElement, { key: "Enter", code: "Enter" });
+    const errorMsg = screen.getByText(/please give a task/i);
+    expect(errorMsg).toBeTruthy();
+  });
 
-test("write something to input and submit it, then delete it", () => {
-  render(<App />);
-  const inputElement = screen.getByPlaceholderText(
-    /new task/i
-  ) as HTMLInputElement;
-
-  expect(inputElement.value).toBe("");
-  fireEvent.change(inputElement, { target: { value: "do this now!" } });
-  expect(inputElement.value).toBe("do this now!");
-  fireEvent.keyDown(inputElement, { key: "Enter", code: "Enter" });
-  let todoTask = screen.getByText(/do this now!/i);
-  expect(todoTask).toBeTruthy();
-  fireEvent.click(screen.getByTitle("remove"));
-  todoTask = screen.queryByText(/do this now!/i) as HTMLInputElement;
-  expect(todoTask).toBeNull(); // it doesn't exist
-
-  expect(inputElement.value).toBe("");
-  fireEvent.change(inputElement, { target: { value: "lol" } });
-  expect(inputElement.value).toBe("lol");
-  fireEvent.keyDown(inputElement, { key: "Enter", code: "Enter" });
-  let lolTask = screen.getByText(/lol/i);
-  expect(lolTask).toBeTruthy();
-  fireEvent.click(screen.getByTitle("remove"));
-  lolTask = screen.queryByText(/lol/i) as HTMLInputElement;
-  expect(lolTask).toBeNull(); // it doesn't exist
+  it("is able to remove the added task", () => {
+    render(<App />);
+    let todoTask = screen.getByText(/task/i);
+    expect(todoTask).toBeTruthy();
+    fireEvent.click(screen.getByTitle("remove"));
+    todoTask = screen.queryByText(/task/i) as HTMLInputElement;
+    expect(todoTask).toBeNull(); // it doesn't exist
+  });
 });
